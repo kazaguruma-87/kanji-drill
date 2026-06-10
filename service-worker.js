c949a8e6047b8e94c941be18be3fe7645ba38523
kazaguruma-87 <tanun222@gmail.com>
2026-06-10 14:15:12 +0900

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,3 +0,0 @@
-// 登録された瞬間に自分自身を強制解雇し、キャッシュを全消去して消滅するコード
-self.addEventListener('install', () => self.skipWaiting());
-self.addEventListener('activate', () => self.registration.unregister().then(() => self.clients.claim()));
